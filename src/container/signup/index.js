0b67897a21fca9375d50e3e3156f4aa180bdad02
sellforsure <fastest2525@gmail.com>
2024-01-11 21:15:39 +0200

    auth-er

--- src/container/signup/index.js
+++ src/container/signup/index.js
@@ -1,18 +1,77 @@
-class SignupForm {
-  static value = {}
+import {
+  Form,
+  REG_EXP_EMAIL,
+  REG_EXP_PASSWORD,
+} from '../../script/form'
 
-  static validate = (name, value) => {
-    return true
+class SignupForm extends Form {
+  FIELD_NAME = {
+    EMAIL: 'email',
+    PASSWORD: 'password',
+    PASSWORD_AGAIN: 'passwordAgain',
+    ROLE: 'role',
+    IS_CONFIRM: 'isConfirm',
+  }
+  FIELD_ERROR = {
+    IS_EMPTY: 'Введіть значення в поле',
+    IS_BIG: 'Дуже довге значення, приберіть зайве',
+    EMAIL: 'Введіть коректне значення e-mail адреси',
+    PASSWORD:
+      'Пароль повинен складатия з не менше ніж 8 символів, включаючи хоча б одну цифру, малу та велику літеру',
+    PASSWORD_AGAIN:
+      'Ваш другий пароль не збігається з першим',
+    NOT_CONFIRM: 'Ви не погоджуєтеся з правилами',
+    ROLE: 'Ви не обрали роль',
   }
 
-  static submit = () => {
+  validate = (name, value) => {
+    if (String(value).length < 1) {
+      return this.FIELD_ERROR.IS_EMPTY
+    }
+
+    if (String(value).length > 20) {
+      return this.FIELD_ERROR.IS_BIG
+    }
+
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(String(value))) {
+        return this.FIELD_ERROR.EMAIL
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD) {
+      if (!REG_EXP_PASSWORD.test(String(value))) {
+        return this.FIELD_ERROR.PASSWORD
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
+      if (
+        String(value) !==
+        this.value[this.FIELD_NAME.PASSWORD]
+      ) {
+        return this.FIELD_ERROR.PASSWORD_AGAIN
+      }
+    }
+
+    if (name === this.FIELD_NAME.IS_CONFIRM) {
+      if (Boolean(value) !== true) {
+        return this.FIELD_ERROR.NOT_CONFIRM
+      }
+    }
+
+    if (name === this.FIELD_NAME.ROLE) {
+      if (isNaN(value)) {
+        return this.FIELD_ERROR.ROLE
+      }
+    }
+  }
+
+  submit = () => {
+    this.validateAll()
+
     console.log(this.value)
-  }
-
-  static change = (name, value) => {
-    console.log(name, value)
-    if (this.validate(name, value)) this.value[name] = value
   }
 }
 
-window.signupForm = SignupForm
+window.signupForm = new SignupForm()
